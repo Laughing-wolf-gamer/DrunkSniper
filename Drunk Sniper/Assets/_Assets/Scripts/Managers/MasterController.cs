9a33df881c57ab1828b2b14fcec21ebb7b9d4c37
Abhishek sinha <73241755+Laughing-wolf-gamer@users.noreply.github.com>
2022-07-01 22:23:31 +0530

    Update 1

---
+++ Drunk Sniper/Assets/_Assets/Scripts/Managers/MasterController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MasterController : MonoBehaviour {
+    
+
+    private void Start() {
+        
+    }
+
+    
+    private void Update() {
+        
+    }
+
+}
